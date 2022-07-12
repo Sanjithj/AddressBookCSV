607602f4c16f172cddb4deacc57f5a8b48b90ff1
Sanjithj <sanjithj591997@gmail.com>
2022-07-12 19:06:30 +0530

    Added: Edit Contact

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,6 +4,7 @@ public class AddressBookMain {
 public static void main(String[] args) {
 	System.out.println("Welcome to address book");
 	AddressBookService.addContact();
+	AddressBookService.editContact();
 	AddressBookService.display();
 
 }
