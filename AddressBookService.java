662a26126d1fc8cec09e4ffbb44c2a2c1add079a
Sanjithj <sanjithj591997@gmail.com>
2022-07-12 19:46:12 +0530

    Added: Duplicate

--- AddressBookService.java
+++ AddressBookService.java
@@ -1,11 +1,12 @@
 package com.bridgelabz;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AddressBookService {
 	static Scanner sc=new Scanner(System.in);
-	static ArrayList<Contacts> arrayOfContacts = new ArrayList<Contacts>();
+	 static List<Contacts> arrayOfContacts = new ArrayList<Contacts>();
 	public static Contacts addContact() {
 		System.out.println("Please enter your first name :");
 	    String first_name = sc.next();
@@ -25,20 +26,25 @@ public class AddressBookService {
 	    System.out.println("Please enter your email id :");
 	    String email = sc.next();
 	    Contacts contact=new Contacts(first_name,last_name,Address,city,state,zip,phone_number,email);
-	   return contact;
-	    
+	    arrayOfContacts.add(contact);
+		return contact;
 	}
 	public static void addContacts() {
 		System.out.println("how many contact you want to add");
 		int n=sc.nextInt();
 		for(int i=0;i<n;i++) {
-			arrayOfContacts.add(addContact());
+			Contacts con=addContact();
+			if(!arrayOfContacts.add(con)) {
+				System.out.println("name already exists");
+				i--;
+				continue;
+			}
 			System.out.println();
 		}
 	}
 	public static int edit(String name) {
 		for(Contacts contact : arrayOfContacts) {
-			if(name.compareToIgnoreCase(Contacts.getFirst_name())==0);{
+			if(name.compareToIgnoreCase(Contacts.getFirst_name())==0){
 				return arrayOfContacts.indexOf(contact);
 			}
 		}
@@ -57,6 +63,9 @@ public class AddressBookService {
 			System.out.println("contact found please edit your contact");
 			addContact();
 		}
+		
+		
+		
 	}
 	public static int delete(String name) {
 		for(Contacts contact : arrayOfContacts) {
@@ -79,16 +88,17 @@ public class AddressBookService {
 			arrayOfContacts.remove(choice);
 			System.out.println("contact deleted successfully!!");
 		}
+		
 	}
 	  public static void display()
 	    {
 
-	        for(Contacts con : arrayOfContacts)
+	        for(Contacts contact : arrayOfContacts)
 	        {
-	            System.out.println(con);
+	            System.out.println(contact);
 	        }
 	    }
 	public static void addAddressBook() {
 	}
-
+	
 }
