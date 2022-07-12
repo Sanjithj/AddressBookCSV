49957b8118d1a3cb7f7a93256ec54dfd36813bc3
Sanjithj <sanjithj591997@gmail.com>
2022-07-12 19:02:33 +0530

    Added: new contacts added

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class AddressBookMain {
 public static void main(String[] args) {
 	System.out.println("Welcome to address book");
+	AddressBookService.addContact();
+	AddressBookService.display();
+
 }
 }
